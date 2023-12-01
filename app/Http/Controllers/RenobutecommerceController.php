990975165a29d1f4eaeb13304533e10f99be77cc
ren21GitHub <reynielobut21@gmail.com>
2023-12-01 17:28:47 +0800

    first push

---
+++ app/Http/Controllers/RenobutecommerceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RenobutecommerceController extends Controller
+{
+    public function index()
+    {
+        return view('renObutECommerce.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Renobutecommerce;
+use App\Http\Controllers\RenobutecommerceController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('renObutECommerce.index');
+Route::controller(RenobutecommerceController::class)->group(function(){
+    Route::get('/', 'index');
+
+});
+
+Route::controller(UserController::class)->group(function(){
+    Route::get('/login', 'login');
+
 });
