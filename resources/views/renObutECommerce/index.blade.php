4f5d3412b57f8e31c304636d6b782b65518966be
ren21GitHub <reynielobut21@gmail.com>
2023-12-12 21:41:33 +0800

    update npm packages

--- resources/views/renObutECommerce/index.blade.php
+++ resources/views/renObutECommerce/index.blade.php
@@ -1,6 +1,6 @@
 @include('partials._header')
     <div {{-- class="bg-white" --}}>
-        <header class="absolute inset-x-0 top-0 z-50">
+        <header class="bg-slate-800 absolute inset-x-0 top-0 z-50">
           <nav class="flex items-center justify-between p-6 sm:px-8" aria-label="Global">
             <div class="flex sm:flex-1">
               <a href="/" class="-m-1.5 p-1.5">
